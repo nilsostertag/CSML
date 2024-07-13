7d61c0484d56ae802ecdb87adab969811a24e86c
nilsostertag <nostertag@gmx.de>
2024-07-13 18:52:54 +0200

    progress comms and backend scripts

--- src/server/data_pipeline.py
+++ src/server/data_pipeline.py
@@ -1,3 +1,15 @@
 import numpy as np
 import pandas as pd
+import utils.comms as cm
 
+def get_from_influx(url: str = 'pi4projects.local', port: str = '1880', request: str = None, only_return_payload: bool = True):
+    response, payload = cm.request(url, port, request)
+    if only_return_payload == False:
+        return response, payload
+    return payload
+
+def normalize_column(column, ref):
+    pass
+
+if __name__ == '__main__':
+    dataset = get_from_influx(request = 'raw_data', only_return_payload = True)
--- src/server/utils/comms.py
+++ src/server/utils/comms.py
@@ -1,4 +1,16 @@
 # Script for REST-Based communication between Python and NodeRED backend
-
+import json
 import urllib.request
+import pandas as pd
 
+def request(url: str = 'pi4projects.local', port: str = '1880', request: str = None):
+    buffer_url = f'{url}:{port}/{request}'
+    response = request(buffer_url)
+    df = pd.read_json(response)
+    return response, df
+
+def push_data(url: str = 'pi4projects.local', port: str = '1880', request: str = None, payload = None):
+    arg = json.dumps(payload)
+    buffer_url = f'{url}:{port}/{request}&arg={arg}'
+    response = request(buffer_url)
+    pass
